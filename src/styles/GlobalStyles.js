23dd2636abca3196456fd2ef6e836f445f73e8db
VictorPioli <victorsnpoliveira@gmail.com>
2025-03-05 22:12:15 -0300

    global styles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    body {
     margin: 0px;
     padding: 0px;
     box-sizing: border-box;
+    }
 `;
